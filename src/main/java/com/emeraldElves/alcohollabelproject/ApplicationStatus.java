b80e64e6dd8d7b96e6aeed337cc5a2cf457d0890
danbmckay <dbmckay@wpi.edu>
2017-04-01 14:52:10 -0400

    Added javadocs for ApplicationStatus

--- src/main/java/com/emeraldElves/alcohollabelproject/ApplicationStatus.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/ApplicationStatus.java
@@ -9,14 +9,26 @@ public enum ApplicationStatus {
     REJECTED("Reason");
     private String message;
 
+    /**
+     * This creates an ApplicationStatus with message
+     * @param message The message associated with the application status
+     */
     ApplicationStatus(String message){
         this.message = message;
     }
-    
+
+    /**
+     * This sets the message for the ApplicationStatus
+     * @param message The message for ApplicationStatus
+     */
     public void setMessage(String message){
         this.message = message;
     }
 
+    /**
+     * This returns the message for application status
+     * @return The message for the application status
+     */
     public String getMessage(){
         return message;
     }
