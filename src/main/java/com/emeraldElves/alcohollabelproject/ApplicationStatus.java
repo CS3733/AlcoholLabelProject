da27ddda14ead7e62897c0cde84c57fddb2bfda0
danbmckay <dbmckay@wpi.edu>
2017-04-01 14:50:10 -0400

    Added ApplicationStatus Enum

--- src/main/java/com/emeraldElves/alcohollabelproject/ApplicationStatus.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/ApplicationStatus.java
@@ -4,5 +4,20 @@ package com.emeraldElves.alcohollabelproject;
  * Created by elijaheldredge on 3/31/17.
  */
 public enum ApplicationStatus {
+    APPROVED("Your application was approved"),
+    PENDINGREVIEW("Pending Review"),
+    REJECTED("Reason");
+    private String message;
 
+    ApplicationStatus(String message){
+        this.message = message;
+    }
+    
+    public void setMessage(String message){
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
 }
