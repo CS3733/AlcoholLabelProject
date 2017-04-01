38c63a2f9dbfb35c936761aedf16756bc4124ebd
danbmckay <dbmckay@wpi.edu>
2017-04-01 15:28:53 -0400

    Started ApplicationController
    Switching to Elijah as driver

---
+++ src/main/java/com/emeraldElves/alcohollabelproject/ApplicationController.java
@@ -0,0 +1,16 @@
+package com.emeraldElves.alcohollabelproject;
+
+import javafx.scene.control.Button;
+
+/**
+ * Created by Dan on 4/1/2017.
+ */
+public class ApplicationController {
+    public Label applicantNameLabel;
+    public Button logoutBtn;
+    
+
+    public ApplicationController(){
+
+    }
+}
