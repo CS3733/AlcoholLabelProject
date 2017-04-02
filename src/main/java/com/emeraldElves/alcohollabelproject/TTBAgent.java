cd87c6a5b95a063297df033dea16fd5aa5829ca8
jsbremner <jsbremner@wpi.edu>
2017-04-02 15:32:38 -0400

    created TTBAgent

---
+++ src/main/java/com/emeraldElves/alcohollabelproject/TTBAgent.java
@@ -0,0 +1,41 @@
+package com.emeraldElves.alcohollabelproject;
+
+/**
+ * Created by Joe on 4/2/2017.
+ */
+
+import java.util.ArrayList;
+
+public class TTBAgent {
+    private String name;
+    private int governmentID;
+    private ArrayList currentApps;
+
+    public TTBAgent() {
+        currentApps = new ArrayList<SubmittedApplication>;
+    }
+
+    /*
+     * getName(): get the name of the TTB agent
+     * @return name of the TTB agent
+     */
+    String getName() {
+        return name;
+    }
+
+    /*
+     * getGovernmentID(): get the TTB agent's government ID
+     * @return government ID of the TTB agent
+     */
+    int getGovernmentID() {
+        return governmentID;
+    }
+
+    /*
+     * getCurrentApplications(): get list of applications TTB agent is currently working on
+     * @return ArrayList of current applications
+     */
+    ArrayList<SubmittedApplication> getCurrentApplications() {
+        return currentApps;
+    }
+}
