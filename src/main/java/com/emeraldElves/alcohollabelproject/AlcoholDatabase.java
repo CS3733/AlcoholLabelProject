2ef2e424247c01a614e2b12f049093ce91959889
Kyle Corry <kylecorry31@gmail.com>
2017-04-02 14:43:22 -0400

    Change ApplicationInfo to SubmittedApplication

--- src/main/java/com/emeraldElves/alcohollabelproject/AlcoholDatabase.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/AlcoholDatabase.java
@@ -25,7 +25,7 @@ public class AlcoholDatabase {
      * @param numApplications The maximum number of applications to receive.
      * @return A list of the most recently approved applications ordered from most recent to least recent.
      */
-    public List<ApplicationInfo> getMostRecentApproved(int numApplications) {
+    public List<SubmittedApplicaiton> getMostRecentApproved(int numApplications) {
 
         return null;
     }
@@ -36,7 +36,7 @@ public class AlcoholDatabase {
      * @param brandName The brand name to search.
      * @return A list of approved alcohols containing the brandName ordered by time approved.
      */
-    public List<ApplicationInfo> searchByBrandName(String brandName) {
+    public List<SubmittedApplication> searchByBrandName(String brandName) {
 
         return null;
     }
@@ -47,7 +47,7 @@ public class AlcoholDatabase {
      * @param application The application to submit. If this is an update to an application, be sure to include the correct unique ID number in the application.
      * @return True if the application was submitted without error.
      */
-    public boolean submitApplication(ApplicationInfo application) {
+    public boolean submitApplication(SubmittedApplication application) {
 
         return false;
     }
@@ -58,7 +58,7 @@ public class AlcoholDatabase {
      * @param numApplications The maximum number of applications to receive.
      * @return A list of the most recent unapproved applications ordered from most recent to least recent.
      */
-    public List<ApplicationInfo> getMostRecentUnapproved(int numApplications) {
+    public List<SubmittedApplication> getMostRecentUnapproved(int numApplications) {
         return null;
     }
 
@@ -69,8 +69,7 @@ public class AlcoholDatabase {
      * @param status      The new status of the application.
      * @return True if the status was updated without error.
      */
-    public boolean updateApplicationStatus(ApplicationInfo application, ApplicationStatus status) {
-
+    public boolean updateApplicationStatus(SubmittedApplication application, ApplicationStatus status) {
         return false;
     }
 }
