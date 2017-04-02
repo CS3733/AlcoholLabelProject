8202398228f1356ec45939651173bf370ca78a46
Kyle Corry <kylecorry31@gmail.com>
2017-04-02 08:53:53 -0400

    Add Logging class

---
+++ src/main/java/com/emeraldElves/alcohollabelproject/Log.java
@@ -0,0 +1,12 @@
+package com.emeraldElves.alcohollabelproject;
+
+/**
+ * Created by Kylec on 4/2/2017.
+ */
+public class Log {
+
+    public static void console(Object value){
+        System.out.println(value);
+    }
+
+}
