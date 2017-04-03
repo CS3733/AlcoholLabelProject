2d37c4f9f383d9de8caaeb508f25c9fd1f973dd7
broodingcoin <tarek1000100@gmail.com>
2017-04-03 14:34:01 -0400

    finished new app for now, GUI for application updates

--- src/main/java/com/emeraldElves/alcohollabelproject/NewApplicationController.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/NewApplicationController.java
@@ -1,22 +1,28 @@
 package com.emeraldElves.alcohollabelproject;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.emeraldElves.alcohollabelproject.Main.loadFXML;
 
 public class NewApplicationController {
     @FXML
-    TextField reIDNoTextField;
+    TextField repIDNoTextField;
     @FXML
     TextField permitNoTextField;
     @FXML
-    RadioButton productSourceRadios;
+    RadioButton international;
     @FXML
-    RadioButton productTypeRadios;
+    RadioButton domestic;
+    @FXML
+    RadioButton beer;
+    @FXML
+    RadioButton wine;
+    @FXML
+    TextField alcoholName;
     @FXML
     TextField brandNameField;
     @FXML
@@ -40,10 +46,108 @@ public class NewApplicationController {
     @FXML
     Button cancelApplication;
     @FXML
-    Button Submit;
+    Button submit;
+    @FXML
+    Label error;
 
     public void submitApp() {
+
+        ToggleGroup productType = new ToggleGroup();
+        beer.setToggleGroup(productType);
+        wine.setToggleGroup(productType);
+        ToggleGroup productSource = new ToggleGroup();
+        international.setToggleGroup(productSource);
+        domestic.setToggleGroup(productSource);
+
+        if(repIDNoTextField.getText() == null) {
+            error.setText("Please fill in the representative ID");
+            return;
+        }
+        if(permitNoTextField.getText() == null) {
+            error.setText("Please fill in the Permit Number");
+            return;
+        }
+        if(productType.getSelectedToggle() == null) {
+            error.setText("Please select Whether your product is international or domestic");
+            return;
+        }
+        if(productSource.getSelectedToggle() == null) {
+            error.setText("Please select what Type of Alcohol you are selling");
+            return;
+        }
+        if(alcoholName.getText() == null) {
+            error.setText("Please fill in your alcohols name");
+            return;
+        }
+        if(brandNameField.getText() == null) {
+            error.setText("Please fill in your brand name");
+            return;
+        }
+        if(addressField.getText() == null) {
+            error.setText("Please fill in the physical address of your company");
+            return;
+        }
+        if(phoneNumberField.getText() == null) {
+            error.setText("Please fill in the contact number");
+            return;
+        }
+        if(emailAddressField.getText() == null) {
+            error.setText("Please fill in the contact email");
+            return;
+        }
+        if(signatureField.getText() == null) {
+            error.setText("Please fill in the signature field");
+            return;
+        }
+        if(datePicker == null) {
+            error.setText("Please fill in the date of your alcohol");
+            return;
+        }
+        if(alcoholContentField.getText() == null) {
+            error.setText("Please fill in the alcohol content of your beverage");
+            return;
+        }
+        if(productType.getSelectedToggle() == wine && wineVintageYearField.getText() == null) {
+            error.setText("Please fill in the wine vintage year");
+            return;
+        }
+        if(productType.getSelectedToggle() == wine && pHLevelField.getText() == null) {
+            error.setText("Please fill in the ph level");
+            return;
+        }
+
         AlcoholDatabase alcoholDB = new AlcoholDatabase(Main.database);
+        EmailAddress appEmail = new EmailAddress(emailAddressField.getText());
+        PhoneNumber appPhone = new PhoneNumber(phoneNumberField.getText());
+        ManufacturerInfo appManInfo = new ManufacturerInfo("placeholder", addressField.getText(), brandNameField.getText(),
+                Integer.parseInt(repIDNoTextField.getText()), Integer.parseInt(permitNoTextField.getText()), appPhone, appEmail);
+        ProductSource pSource = ProductSource.DOMESTIC;
+        AlcoholInfo appAlcoholInfo =  new AlcoholInfo(Integer.parseInt(alcoholContentField.getText()), alcoholName.getText(), brandNameField.getText(), pSource);
+
+        if(productSource.getSelectedToggle() == domestic) {
+            pSource = ProductSource.DOMESTIC;
+        }
+        if(productSource.getSelectedToggle() == international) {
+            pSource = ProductSource.IMPORTED;
+        }
+        if(productType.getSelectedToggle() == beer) {
+            appAlcoholInfo = new BeerInfo(Integer.parseInt(alcoholContentField.getText()), alcoholName.getText(), brandNameField.getText(), pSource);
+        }
+        if(productType.getSelectedToggle() == wine) {
+            appAlcoholInfo = new WineInfo(Integer.parseInt(alcoholContentField.getText()), alcoholName.getText(), brandNameField.getText(), pSource,
+                    Integer.parseInt(wineVintageYearField.getText()), Integer.parseInt(pHLevelField.getText()));
+        }
+
+        java.sql.Date newDate = java.sql.Date.valueOf(datePicker.getValue());
+        ApplicationInfo appInfo = new ApplicationInfo(newDate, appManInfo, appAlcoholInfo);
+
+        ApplicationStatus placeholder = ApplicationStatus.APPROVED;
+
+        List<SubmittedApplication> appliers = new ArrayList();
+        Applicant applicant = new Applicant(appliers);
+
+        SubmittedApplication newApp = new SubmittedApplication(appInfo, placeholder, applicant);
+        alcoholDB.submitApplication(newApp);
     }
 
     public void cancelApp() {
