ae107868a0e4a597b5362ad6b5b1d0d0e2f633e8
keionbis <keion96@gmail.com>
2017-04-03 18:58:04 -0400

    Implemented submission

--- src/main/java/com/emeraldElves/alcohollabelproject/UpdateApplicationController.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/UpdateApplicationController.java
@@ -1,14 +1,15 @@
 package com.emeraldElves.alcohollabelproject;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import com.emeraldElves.alcohollabelproject.ApplicationStatus;
 import com.emeraldElves.alcohollabelproject.ProductSource;
 import com.emeraldElves.alcohollabelproject.AlcoholType;
 import com.emeraldElves.alcohollabelproject.SubmittedApplication;
-
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+import java.time.Instant;
 /**
  * Created by Keion Bisland on 4/2/2017.
  */
@@ -16,7 +17,9 @@ import com.emeraldElves.alcohollabelproject.SubmittedApplication;
 public class UpdateApplicationController {
 
    public ApplicationStatus status;//get status from database
-
+    @FXML
+    ToggleGroup ProductSourceGroup;
+    ToggleGroup ProductTypeGroup;
     @FXML
     TextField repIDNoTextField;
     @FXML
@@ -36,6 +39,8 @@ public class UpdateApplicationController {
     @FXML
     TextField phLevelField;
     @FXML
+    TextField fancifulName;
+    @FXML
     TextField signatureField;
 
     @FXML
@@ -46,13 +51,13 @@ public class UpdateApplicationController {
     RadioButton ProductType_Beer;
     @FXML
     RadioButton ProductType_Wine;
-
+    @FXML
+    Button submitBtn;
     @FXML
     DatePicker datePicker;
 
 
-    public void ApplicationStatuschecker()
-    {
+    public void ApplicationStatuschecker() {
         switch(status)
         {
             case REJECTED:
@@ -70,8 +75,7 @@ public class UpdateApplicationController {
     }
 
 
-    public void updateRejected()
-    {
+    public void updateRejected(){
         SubmittedApplication CurrentlyBeingUpdated = null;
 
         repIDNoTextField.setText(String.valueOf(CurrentlyBeingUpdated.getApplication().getManufacturer().getRepresentativeID()));
@@ -81,7 +85,6 @@ public class UpdateApplicationController {
         phoneNumberField.setText(String.valueOf(CurrentlyBeingUpdated.getApplication().getManufacturer().getPhoneNumber()));
         emailAddressField.setText(String.valueOf(CurrentlyBeingUpdated.getApplication().getManufacturer().getEmailAddress()));
         alcoholContentField.setText(String.valueOf(CurrentlyBeingUpdated.getApplication().getAlcohol().getAlcoholContent()));
-        datePicker.setPromptText(String.valueOf(CurrentlyBeingUpdated.getApplication().getSubmissionDate()));
         if(CurrentlyBeingUpdated.getApplication().getAlcohol().getOrigin() == ProductSource.IMPORTED)
         {
             InternationalRadio.setSelected(true);
@@ -93,8 +96,8 @@ public class UpdateApplicationController {
         if(CurrentlyBeingUpdated.getApplication().getAlcohol().getAlcoholType() == AlcoholType.WINE)
         {
             ProductType_Wine.setSelected(true);
-            wineVintageYearField.setText(String.valueOf(CurrentlyBeingUpdated.getApplication().getAlcohol().getWineInfo().getVintageYear()));
-            phLevelField.setText(String.valueOf(CurrentlyBeingUpdated.getApplication().getAlcohol().getWineInfo().getPH()));
+            wineVintageYearField.setText(String.valueOf(CurrentlyBeingUpdated.getApplication().getAlcohol().getWineInfo().vintageYear));
+            phLevelField.setText(String.valueOf(CurrentlyBeingUpdated.getApplication().getAlcohol().getWineInfo().pH));
         }
         else if(CurrentlyBeingUpdated.getApplication().getAlcohol().getAlcoholType() == AlcoholType.BEER)
         {
@@ -104,10 +107,10 @@ public class UpdateApplicationController {
 
     }
 
-    public void updateApproved()
-    {
+    public void updateApproved() {
         SubmittedApplication CurrentlyBeingUpdated = null;
-
+        Date date = new Date();
+        Instant instant = (CurrentlyBeingUpdated.getApplication().getSubmissionDate().toInstant());
         repIDNoTextField.setText(String.valueOf(CurrentlyBeingUpdated.getApplication().getManufacturer().getRepresentativeID()));
         permitNoTextField.setText(String.valueOf(CurrentlyBeingUpdated.getApplication().getManufacturer().getPermitNum()));
         brandNameField.setText(String.valueOf(CurrentlyBeingUpdated.getApplication().getAlcohol().getBrandName()));
@@ -115,7 +118,6 @@ public class UpdateApplicationController {
         phoneNumberField.setText(String.valueOf(CurrentlyBeingUpdated.getApplication().getManufacturer().getPhoneNumber()));
         emailAddressField.setText(String.valueOf(CurrentlyBeingUpdated.getApplication().getManufacturer().getEmailAddress()));
         alcoholContentField.setText(String.valueOf(CurrentlyBeingUpdated.getApplication().getAlcohol().getAlcoholContent()));
-        datePicker.setPromptText(String.valueOf(CurrentlyBeingUpdated.getApplication().getSubmissionDate()));
         if(CurrentlyBeingUpdated.getApplication().getAlcohol().getOrigin() == ProductSource.IMPORTED)
         {
             InternationalRadio.setSelected(true);
@@ -127,17 +129,64 @@ public class UpdateApplicationController {
         if(CurrentlyBeingUpdated.getApplication().getAlcohol().getAlcoholType() == AlcoholType.WINE)
         {
             ProductType_Wine.setSelected(true);
-            wineVintageYearField.setText(String.valueOf(CurrentlyBeingUpdated.getApplication().getAlcohol().getWineInfo().getVintageYear()));
-            phLevelField.setText(String.valueOf(CurrentlyBeingUpdated.getApplication().getAlcohol().getWineInfo().getPH()));
+            wineVintageYearField.setText(String.valueOf(CurrentlyBeingUpdated.getApplication().getAlcohol().getWineInfo().vintageYear));
+            phLevelField.setText(String.valueOf(CurrentlyBeingUpdated.getApplication().getAlcohol().getWineInfo().pH));
         }
         else if(CurrentlyBeingUpdated.getApplication().getAlcohol().getAlcoholType() == AlcoholType.BEER)
         {
             ProductType_Beer.setSelected(true);
         }
 
-       // CurrentlyBeingUpdated.application.manufacturer.setRepresentativeID(repIDNoTextField.getText());
+
     }
 
+    public void SubmitUpdateApplication(){
+        String applicantname = null;
+        String physicalAddress = AddressField.getText();
+        String company = null;
+        int representativeID = Integer.parseInt(repIDNoTextField.getText());
+        int permitNum = Integer.parseInt(permitNoTextField.getText());
+        PhoneNumber phoneNumber = new PhoneNumber(phoneNumberField.getText());
+        EmailAddress emailAddress = new EmailAddress(emailAddressField.getText());
+        String fancifulname = fancifulName.getText();
+        String brandName = brandNameField.getText();
+        int pH = Integer.parseInt(phLevelField.getText()) ;
+        int vintageYear = Integer.parseInt(wineVintageYearField.getText());
+        int alcoholContent = Integer.parseInt(alcoholContentField.getText());
+        ProductSource origin = null;
+        AlcoholType alcoholType = null;
+        AlcoholInfo.Wine wineInfo = null;
+        if(ProductSourceGroup.getSelectedToggle() == InternationalRadio) {
+             origin = ProductSource.IMPORTED;
+        }
+        else if(ProductSourceGroup.getSelectedToggle() == DomesticRadio) {
+             origin = ProductSource.DOMESTIC;
+        }
+        if(ProductTypeGroup.getSelectedToggle() == ProductType_Beer) {
+            alcoholType= AlcoholType.BEER;
+             wineInfo = null;
+        }
+        else if(ProductTypeGroup.getSelectedToggle() == ProductType_Wine) {
+             alcoholType = AlcoholType.WINE;
+             wineInfo = new AlcoholInfo.Wine(vintageYear,pH);
+        }
 
+
+        Date submissionDate = java.sql.Date.valueOf(String.valueOf(datePicker.getValue())) ;
+
+        ManufacturerInfo manufacturer =  new  ManufacturerInfo(applicantname, physicalAddress, company, representativeID, permitNum, phoneNumber, emailAddress);
+
+
+        ApplicationStatus status = null;
+        AlcoholInfo submittedAlcohol = new AlcoholInfo( alcoholContent,  fancifulname, brandName, origin, alcoholType, wineInfo);
+
+        ApplicationInfo application = new ApplicationInfo( submissionDate,  manufacturer,  submittedAlcohol);
+
+
+
+        Applicant applicant;
+        SubmittedApplication CurrentlyBeingUpdated = null;
+
+    }
 
 }
