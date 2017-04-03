32b6be6e44cedf411badc26d741dc1d94f7992a4
Kyle Corry <kylecorry31@gmail.com>
2017-04-03 18:55:09 -0400

    minor performance changes

--- src/main/java/com/emeraldElves/alcohollabelproject/Main.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/Main.java
@@ -48,6 +48,20 @@ public class Main extends Application {
             Log.console("Used existing SubmittedApplications table");
         }
 
+        try {
+            database.createTable("SubmittedApplications", new Database.TableField("applicationID", "INTEGER UNIQUE NOT NULL"),
+                    new Database.TableField("applicantID", "INTEGER NOT NULL"),
+                    new Database.TableField("status", "INTEGER NOT NULL"),
+                    new Database.TableField("statusMsg", "VARCHAR (10000)"),
+                    new Database.TableField("submissionTime", "TIMESTAMP"),
+                    new Database.TableField("expirationDate", "TIMESTAMP"),
+                    new Database.TableField("agentName", "VARCHAR (255)"),
+                    new Database.TableField("approvalDate", "TIMESTAMP"));
+            Log.console("Created new ManufacturerInfo table");
+        } catch (SQLException e) {
+            Log.console("Used existing ManufacturerInfo table");
+        }
+
 
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/mainGUI.fxml"));
         primaryStage.setTitle("Alcohol Label Project");
