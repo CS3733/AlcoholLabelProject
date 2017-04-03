a05d00206ab9bde42ed96622d40bc81b10a48eea
Zhihao Xie <harrychia97@gmail.com>
2017-04-03 18:02:41 -0400

    created workflowcontroller

---
+++ src/main/java/com/emeraldElves/alcohollabelproject/WorkflowController.java
@@ -0,0 +1,20 @@
+package com.emeraldElves.alcohollabelproject;
+
+/**
+ * Created by Harry on 4/3/2017.
+ */
+
+import javafx.scene.control.*;
+
+public class WorkflowController {
+    public Label ttbAgentNameLabel;
+    public ComboBox pullApplicationsDropDown;
+    public Button logoutBtn;
+    public Button viewCompletedApplicationBtn;
+    public ComboBox statusDropdown;
+    public Label alcoholNameLabel;
+    public Label applicatorNameLabel;
+    public Label dateLabel;
+    public Button submitBtn;
+    
+}
