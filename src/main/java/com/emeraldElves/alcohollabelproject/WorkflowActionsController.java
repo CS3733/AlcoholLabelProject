2bfd657f6dc2744ee3247f2c0e3780ea121fc396
keionbis <keion96@gmail.com>
2017-04-04 12:26:26 -0400

    Added workflowActionController

---
+++ src/main/java/com/emeraldElves/alcohollabelproject/WorkflowActionsController.java
@@ -0,0 +1,27 @@
+package com.emeraldElves.alcohollabelproject;
+
+
+/**
+ * Created by keionbis on 4/4/17.
+ */
+public class WorkflowActionsController {
+    public void approve() {
+
+
+    }
+    public void approveWithConditions() {
+
+    }
+    public void needsCorrections(){
+
+    }
+    public void reject(){
+
+
+    }
+    public void forwardApplication() {
+        
+    }
+
+}
+
