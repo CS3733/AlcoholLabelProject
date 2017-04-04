0c08824380e628e377a4c30bf81a354d054fee86
jsbremner <jsbremner@wpi.edu>
2017-04-04 15:27:31 -0400

    minor changes

--- src/main/java/com/emeraldElves/alcohollabelproject/WorkflowController.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/WorkflowController.java
@@ -4,19 +4,29 @@ package com.emeraldElves.alcohollabelproject;
  * Created by Harry on 4/3/2017.
  */
 
+import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import java.util.ArrayList;
 
 
 public class WorkflowController {
+    @FXML
     public Label ttbAgentNameLabel;
+    @FXML
     public ComboBox pullApplicationsDropDown;
+    @FXML
     public Button logoutBtn;
+    @FXML
     public Button viewCompletedApplicationBtn;
+    @FXML
     public ComboBox statusDropdown;
+    @FXML
     public Label alcoholNameLabel;
+    @FXML
     public Label applicatorNameLabel;
+    @FXML
     public Label dateLabel;
+    @FXML
     public Button submitBtn;
 
     public void queryDatabase()
