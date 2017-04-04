6045f26d0193cf985742cfa7c5c07eb8c02bcaca
keionbis <keion96@gmail.com>
2017-04-04 12:40:23 -0400

    Populated WorkflowActionController functions

--- src/main/java/com/emeraldElves/alcohollabelproject/WorkflowActionsController.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/WorkflowActionsController.java
@@ -5,22 +5,33 @@ package com.emeraldElves.alcohollabelproject;
  * Created by keionbis on 4/4/17.
  */
 public class WorkflowActionsController {
-    public void approve() {
 
+    public void approve() {
+        AlcoholDatabase alcoholDatabase = null;
+        SubmittedApplication ApplicationUnderReview = null;
+        alcoholDatabase.updateApplicationStatus(ApplicationUnderReview ,ApplicationStatus.APPROVED);
 
     }
     public void approveWithConditions() {
-
+        AlcoholDatabase alcoholDatabase = null;
+        SubmittedApplication ApplicationUnderReview = null;
+        alcoholDatabase.updateApplicationStatus(ApplicationUnderReview ,ApplicationStatus.APPROVEDWITHCONDITIONS);
     }
     public void needsCorrections(){
-
+        AlcoholDatabase alcoholDatabase = null;
+        SubmittedApplication ApplicationUnderReview = null;
+        alcoholDatabase.updateApplicationStatus(ApplicationUnderReview ,ApplicationStatus.NEEDSCORRECTIONS);
     }
     public void reject(){
-
+        AlcoholDatabase alcoholDatabase = null;
+        SubmittedApplication ApplicationUnderReview = null;
+        alcoholDatabase.updateApplicationStatus(ApplicationUnderReview ,ApplicationStatus.REJECTED);
 
     }
     public void forwardApplication() {
-        
+        AlcoholDatabase alcoholDatabase = null;
+        SubmittedApplication ApplicationUnderReview = null;
+        alcoholDatabase.updateApplicationStatus(ApplicationUnderReview ,ApplicationStatus.PENDINGREVIEW);
     }
 
 }
