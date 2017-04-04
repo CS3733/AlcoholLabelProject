0d85298289bc23ceae081d51683f96a2876d4cad
Jessie Ying <jessie.ying@gmail.com>
2017-04-04 15:42:26 -0400

    Fixed error fields and data for page 2
    date check if empty doesn’t work and I still need to write check for if
    text in fields are integers

--- src/main/java/com/emeraldElves/alcohollabelproject/NewApplicationController.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/NewApplicationController.java
@@ -68,54 +68,59 @@ public class NewApplicationController {
     //Creates a database to store the alcohol information from form
     AlcoholDatabase alcoholDB = new AlcoholDatabase(Main.database);
 
-    //Stores representative ID number
+    //Initializes and temporarily stores applicant's info
     int repIDNo = -1; //means they didn't enter a rep ID num
-    //Stores the permit number
     int permitNo = 0;
-    //Stores the physical address of the applicant/company
     String physicalAddress= null;
-    //Stores the applicant's email
     EmailAddress applicantEmail = null;
-    //Stores the applicant's phone number
     PhoneNumber applicantPhone = null;
+
     //Stores the manufacturer's info
     ManufacturerInfo appManInfo = null;
+
+    //Initializes and temporarily stores data fields for alc info
+    ProductSource pSource = null;
+    AlcoholType alcType = null;
+    String alcName = null;
+    String brandName = null;
+    int alcContent = 0;
+    AlcoholInfo.Wine wineType = null; //null if type is beer (this doesn't get edited)
+
+    //Stores the alcohol info from the form
+    AlcoholInfo appAlcoholInfo = null;
 
 
     public void nextPage(){
 
         Boolean formFilled=false;
 
+        //errors are printed if required fields are not filled in
         if(permitNoTextField.getText().isEmpty()) {
             permitNoErrorField.setText("Please fill in your permit number.");
-            //return;
         }
         if(addressField.getText().isEmpty()) {
             addressErrorField.setText("Please fill in the physical address of your company.");
-            //return;
         }
         if(phoneNumberField.getText().isEmpty()) {
             phoneNumErrorField.setText("Please fill in the contact number.");
-            //return;
         }
         if(emailAddressField.getText().isEmpty()) {
             emailErrorField.setText("Please fill in the contact email.");
-            //return;
         }
 
+        //check if required fields are filled
         if(!emailAddressField.getText().isEmpty()&&!phoneNumberField.getText().isEmpty()&&
                 !addressField.getText().isEmpty()&&!permitNoTextField.getText().isEmpty()){
             formFilled=true;
         }
-        if(formFilled){
-            //Store the applicant information now that the form is filled
+        if(formFilled) { //Store the applicant information now that the form is filled
 
             //NEED TO CHECK IF THIS IS ACTUALLY AN INT!!!
             if(!repIDNoTextField.getText().isEmpty()) {
                 repIDNo=Integer.parseInt(repIDNoTextField.getText());
             }
             //Parses text from permit number field and stores it as an int
-            //permitNo=Integer.parseInt(permitNoTextField.getText());
+            permitNo=Integer.parseInt(permitNoTextField.getText()); //CHECK INT!!!
 
             //Gets text from email address field and stores it as a string
             physicalAddress=addressField.getText();
@@ -126,126 +131,119 @@ public class NewApplicationController {
             //Gets text from the email address field and stores it as a PhoneNumber
             applicantPhone = new PhoneNumber(phoneNumberField.getText());
 
-            //also check if emails or phone numbers are valid!
+            //also check if emails or phone numbers are valid!!!!!!
 
             //form is now filled in so go to page 2 of label application
             Main.loadFXML("/fxml/new-app-page2.FXML");
         }
     }
 
+
     public void submitApp() {
-        /*radio buttons need to be put in a group so that they can function, these two groups are for
-        product source and the product type radio buttons.
-         */
+
+        Boolean formFilled=false;
+
+        //radio buttons are grouped in the following groups:
+        //product type group
         ToggleGroup productType = new ToggleGroup();
         beer.setToggleGroup(productType);
         wine.setToggleGroup(productType);
 
+        //product source group
         ToggleGroup productSource = new ToggleGroup();
         international.setToggleGroup(productSource);
         domestic.setToggleGroup(productSource);
 
-        /*These next few if statements are all checking to see if there is anything in the application that hasnt been filled out that
-        needs to be filled out. for the fields specific to wine, it will only throw an error if wine is selected in the product type
-        radio button group
-         */
-        if(productType.getSelectedToggle() == null) {
-            error2.setText("Please select Whether your product is international or domestic");
-            return;
-        }
+        //errors are printed if required fields are not filled in
         if(productSource.getSelectedToggle() == null) {
-            error2.setText("Please select what Type of Alcohol you are selling");
-            return;
+            error2.setText("Please select whether the alcohol is imported or domestic.");
         }
-        if(alcoholName.getText().isEmpty()) {
-            error2.setText("Please fill in your alcohols name");
-            return;
+        if(productType.getSelectedToggle() == null) {
+            error2.setText("Please select whether the alcohol is a beer or wine.");
         }
         if(brandNameField.getText().isEmpty()) {
-            error2.setText("Please fill in your brand name");
-            return;
+            error2.setText("Please fill in the brand name.");
         }
         if(alcoholContentField.getText().isEmpty()) {
-            error2.setText("Please fill in the alcohol content of your beverage");
-            return;
+            error2.setText("Please fill in the alcohol content (as a %) of the beverage.");
         }
-        if(productType.getSelectedToggle() == wine && wineVintageYearField.getText().isEmpty()) {
-            error2.setText("Please fill in the wine vintage year");
-            return;
+        if (productType.getSelectedToggle() == wine){
+            int vintageYr=0;
+            double pH=0.0;
+            if(!wineVintageYearField.getText().isEmpty()) {
+                vintageYr=Integer.parseInt(wineVintageYearField.getText()); //CHECK IF INPUT INTEGER!
+            }
+            if(!pHLevelField.getText().isEmpty()) {
+                pH=Double.parseDouble(pHLevelField.getText()); //CHECK IF INPUT INTEGER!
+            }
+            wineType = new AlcoholInfo.Wine(pH, vintageYr);
         }
-        if(productType.getSelectedToggle() == wine && pHLevelField.getText().isEmpty()) {
-            error2.setText("Please fill in the ph level");
-            return;
-        }
+
         if(datePicker == null) {
-            error2.setText("Please fill in the date of your alcohol");
-            return;
+            error2.setText("Please select the date.");
         }
         if(signatureField.getText().isEmpty()) {
-            error2.setText("Please fill in the signature field");
-            return;
+            error2.setText("Please fill in the signature field.");
         }
 
-
-
-        //Creates a database to store the alcohol information from form
-        AlcoholDatabase alcoholDB = new AlcoholDatabase(Main.database);
-
-        //Parses the text from the email address field and stores it as an EmailAddress
-        EmailAddress applicantEmail = new EmailAddress(emailAddressField.getText());
-
-        //Parses the text from the email address field and stores it as a PhoneNumber
-        PhoneNumber applicantPhone = new PhoneNumber(phoneNumberField.getText());
-
-        //Creates a ManufacturerInfo from the address, brand
-        ManufacturerInfo appManInfo = new ManufacturerInfo("Person", addressField.getText(), brandNameField.getText(),
-                Integer.parseInt(repIDNoTextField.getText()), Integer.parseInt(permitNoTextField.getText()),
-                applicantPhone, applicantEmail);
-
-        /*
-        The next 5 lines of code are all placeholders that may or may not be edited depending on if certain conditions
-        are met
-         */
-        ProductSource pSource = ProductSource.DOMESTIC;
-        AlcoholType mainAlcType = AlcoholType.OTHER;
-        AlcoholInfo.Wine winePlaceholder = new AlcoholInfo.Wine(0, 0);
-
-
-        //Checking if the product is domestic or imported by checking the product source radio button group
-        if(productSource.getSelectedToggle() == domestic) {
-            pSource = ProductSource.DOMESTIC;
-        }
-        if(productSource.getSelectedToggle() == international) {
-            pSource = ProductSource.IMPORTED;
+        //check if required fields are filled in
+        if((productType.getSelectedToggle() != null) && (productSource.getSelectedToggle() != null) &&
+                !brandNameField.getText().isEmpty() && !alcoholContentField.getText().isEmpty() &&
+                (datePicker != null) && !signatureField.getText().isEmpty()){
+            formFilled=true;
         }
 
-        if(productType.getSelectedToggle() == beer) {
-            mainAlcType = AlcoholType.BEER;
+        if(formFilled) {
+            //Checking if the product is domestic or imported by checking the product source radio button group
+            if (productSource.getSelectedToggle() == domestic) {
+                pSource = ProductSource.DOMESTIC;
+            }
+            if (productSource.getSelectedToggle() == international) {
+                pSource = ProductSource.IMPORTED;
+            }
+
+            //Checking if the product is a beer or wine by checking the product source radio button group
+            if (productType.getSelectedToggle() == beer) {
+                alcType = AlcoholType.BEER;
+            }
+            if (productType.getSelectedToggle() == wine) {
+                alcType = AlcoholType.WINE;
+            }
+
+            //sets alc info fields
+            alcName = alcoholName.getText();
+            brandName = brandNameField.getText();
+            alcContent = Integer.parseInt(alcoholContentField.getText()); //CHECK IF INTEGER
+
+            //sets the alcohol info
+            appAlcoholInfo = new AlcoholInfo(alcContent, alcName, brandName, pSource, alcType, wineType);
+
+            //creates and sets the date value
+            java.sql.Date newDate = java.sql.Date.valueOf(datePicker.getValue());
+
+            // Creates a new application info and sets data
+            ApplicationInfo appInfo = new ApplicationInfo(newDate, appManInfo, appAlcoholInfo);
+
+            //!!!!!placeholder for applicant's submitted applications!!!!!
+            List<SubmittedApplication> appList = new ArrayList();
+            //appList.add(newApp); <---need to be able to add this application to appList in workflow
+
+            //Create applicant to store in submitted application
+            Applicant applicant = new Applicant(appList);
+
+            //Create a SubmittedApplication
+            SubmittedApplication newApp = new SubmittedApplication(appInfo, ApplicationStatus.PENDINGREVIEW, applicant);
+
+            //Submit the new application to the database
+            alcoholDB.submitApplication(newApp);
+
+            //Go back to homepage
+            Main.loadFXML("/fxml/mainGUI.FXML");
         }
-        if(productType.getSelectedToggle() == wine) {
-            mainAlcType = AlcoholType.WINE;
-            winePlaceholder = new AlcoholInfo.Wine(Double.parseDouble(pHLevelField.getText()), Integer.parseInt(wineVintageYearField.getText()));
-        }
-
-        AlcoholInfo appAlcoholInfo =  new AlcoholInfo(Integer.parseInt(alcoholContentField.getText()), alcoholName.getText(), brandNameField.getText(),
-                pSource, mainAlcType, winePlaceholder);
-
-        java.sql.Date newDate = java.sql.Date.valueOf(datePicker.getValue());
-        ApplicationInfo appInfo = new ApplicationInfo(newDate, appManInfo, appAlcoholInfo);
-
-        ApplicationStatus placeholder = ApplicationStatus.APPROVED;
-
-        List<SubmittedApplication> appliers = new ArrayList();
-        Applicant applicant = new Applicant(appliers);
-
-        SubmittedApplication newApp = new SubmittedApplication(appInfo, placeholder, applicant);
-        alcoholDB.submitApplication(newApp);
-
-        Main.loadFXML("/fxml/mainGUI.FXML");
     }
 
     public void cancelApp() {
-
+        //Go back to homepage
         Main.loadFXML("/fxml/mainGUI.FXML");
     }
     public void saveApp() {
