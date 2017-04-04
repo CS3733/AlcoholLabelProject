e1a774058777e48045b147bf0f1eb6e43093490a
Kyle Corry <kylecorry31@gmail.com>
2017-04-03 22:06:25 -0400

    Switch AlcoholDatabaseTest to DatabaseController

--- src/test/java/com/emeraldElves/alcohollabelproject/AlcoholDatabaseTest.java
+++ src/test/java/com/emeraldElves/alcohollabelproject/AlcoholDatabaseTest.java
@@ -17,66 +17,7 @@ public class AlcoholDatabaseTest {
 
     @Before
     public void setup() {
-        db = new Database("testDB");
-        db.connect();
-        try {
-            db.createTable("TTBAgentLogin", new Database.TableField("username", "VARCHAR (255) UNIQUE NOT NULL"),
-                    new Database.TableField("password", "VARCHAR (255) NOT NULL"));
-            Log.console("Created new TTBAgentLogin table");
-        } catch (SQLException e) {
-            Log.console("Used existing TTBAgentLogin table");
-        }
-
-        try {
-            db.createTable("ApplicantLogin", new Database.TableField("username", "VARCHAR (255) UNIQUE NOT NULL"),
-                    new Database.TableField("password", "VARCHAR (255) NOT NULL"));
-            Log.console("Created new ApplicantLogin table");
-        } catch (SQLException e) {
-            Log.console("Used existing ApplicantLogin table");
-        }
-
-        try {
-            db.createTable("SubmittedApplications", new Database.TableField("applicationID", "INTEGER UNIQUE NOT NULL"),
-                    new Database.TableField("applicantID", "INTEGER NOT NULL"),
-                    new Database.TableField("status", "INTEGER NOT NULL"),
-                    new Database.TableField("statusMsg", "VARCHAR (10000) NOT NULL"),
-                    new Database.TableField("submissionTime", "TIMESTAMP NOT NULL"),
-                    new Database.TableField("expirationDate", "TIMESTAMP NOT NULL"),
-                    new Database.TableField("agentName", "VARCHAR (255) NOT NULL"),
-                    new Database.TableField("approvalDate", "TIMESTAMP NOT NULL"),
-                    new Database.TableField("TTBUsername", "VARCHAR (255) NOT NULL"));
-            Log.console("Created new SubmittedApplications table");
-        } catch (SQLException e) {
-            Log.console("Used existing SubmittedApplications table");
-        }
-
-        try {
-            db.createTable("ManufacturerInfo", new Database.TableField("applicationID", "INTEGER UNIQUE NOT NULL"),
-                    new Database.TableField("authorizedName", "VARCHAR (255) NOT NULL"),
-                    new Database.TableField("physicalAddress", "VARCHAR (255) NOT NULL"),
-                    new Database.TableField("company", "VARCHAR (10000) NOT NULL"),
-                    new Database.TableField("representativeID", "INTEGER NOT NULL"),
-                    new Database.TableField("permitNum", "INTEGER NOT NULL"),
-                    new Database.TableField("phoneNum", "VARCHAR (255) NOT NULL"), //check with kyle
-                    new Database.TableField("emailAddress", "VARCHAR (255) NOT NULL"));
-            Log.console("Created new ManufacturerInfo table");
-        } catch (SQLException e) {
-            Log.console("Used existing ManufacturerInfo table");
-        }
-
-        try {
-            db.createTable("AlcoholInfo", new Database.TableField("applicationID", "INTEGER UNIQUE NOT NULL"),
-                    new Database.TableField("alcoholContent", "DOUBLE NOT NULL"),
-                    new Database.TableField("fancifulName", "VARCHAR (255) NOT NULL"),
-                    new Database.TableField("brandName", "VARCHAR (10000) NOT NULL"),
-                    new Database.TableField("origin", "VARCHAR (255) NOT NULL"),
-                    new Database.TableField("type", "INTEGER NOT NULL"),  //only beer or wine?
-                    new Database.TableField("pH", "DOUBLE"),
-                    new Database.TableField("vintageYear", "VARCHAR (255)"));
-            Log.console("Created new AlcoholInfo table");
-        } catch (SQLException e) {
-            Log.console("Used existing AlcoholInfo table");
-        }
+        db = DatabaseController.getInstance().initDatabase("testDB");
         alcoholDatabase = new AlcoholDatabase(db);
     }
 
