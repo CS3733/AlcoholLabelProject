25ee51fbd7c67bc88618131cf9a1acffe4fc1925
Kyle Corry <kylecorry31@gmail.com>
2017-04-04 22:24:10 -0400

    Remove unused imports

--- src/main/java/com/emeraldElves/alcohollabelproject/ApplicantWorkflowController.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/ApplicantWorkflowController.java
@@ -1,19 +1,7 @@
 package com.emeraldElves.alcohollabelproject;
 
-import javafx.fxml.FXML;
-import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TableRow;
-import javafx.scene.layout.HBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
 
 /**
  * Created by keionbis on 4/4/17.
