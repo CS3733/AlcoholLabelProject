a56589b953bef4e0a00c19e16d460ab8ab00cc1b
jsbremner <jsbremner@wpi.edu>
2017-04-10 14:16:34 -0400

    new branch

--- src/main/java/com/emeraldElves/alcohollabelproject/ApplicationAssigner.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/ApplicationAssigner.java
@@ -6,9 +6,9 @@ import java.util.List;
  * Created by Joe on 4/9/2017.
  */
 public abstract class ApplicationAssigner {
-    private IAssigner assigner;
-    private List<String> agentUserNames;
-    private String lastAssignedAgent;
+    protected static IAssigner assigner;
+    protected List<String> agentUserNames;
+    protected String lastAssignedAgent;
 
     // constructors
     public ApplicationAssigner(IAssigner assigner, List<String> agentUserNames) {
