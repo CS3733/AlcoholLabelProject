61633cbfe324f72deb795fd29fdac6ad2871535a
eralmansouri <eralmansouri@gmail.com>
2017-04-12 10:49:40 -0400

    search filters
    * added filtering to search (rightclick)
    * filter also applies to suggestions

--- src/main/java/com/emeraldElves/alcohollabelproject/UserInterface/SearchController.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/UserInterface/SearchController.java
@@ -1,10 +1,7 @@
 package com.emeraldElves.alcohollabelproject.UserInterface;
 
 import com.emeraldElves.alcohollabelproject.COLASearch;
-import com.emeraldElves.alcohollabelproject.Data.AlcoholDatabase;
-import com.emeraldElves.alcohollabelproject.Data.Storage;
-import com.emeraldElves.alcohollabelproject.Data.SubmittedApplication;
-import com.emeraldElves.alcohollabelproject.Data.UserType;
+import com.emeraldElves.alcohollabelproject.Data.*;
 import javafx.application.Platform;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.value.ObservableValue;
@@ -60,6 +57,13 @@ public class SearchController {
     private MenuItem contextSaveBtn;
     @FXML
     private Label descriptionLabel;
+    @FXML
+    private CheckMenuItem filterBeers;
+    @FXML
+    private CheckMenuItem filterWine;
+    @FXML
+    private CheckMenuItem filterSpirits;
+
     private ObservableList<SubmittedApplication> data = FXCollections.observableArrayList();
     private COLASearch search;
 
@@ -89,7 +93,18 @@ public class SearchController {
         });
         typeCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<SubmittedApplication, String>, ObservableValue<String>>() {
             public ObservableValue<String> call(TableColumn.CellDataFeatures<SubmittedApplication, String> p) {
-                return new ReadOnlyObjectWrapper<String>(StringEscapeUtils.escapeJava(p.getValue().getApplication().getAlcohol().getAlcoholType().name()));
+                String alcoholName = p.getValue().getApplication().getAlcohol().getAlcoholType().name();
+                switch (alcoholName){
+                    case "DISTILLEDSPIRITS":
+                        return new ReadOnlyObjectWrapper<String>("Distilled Spirit");
+                    case "WINE":
+                        return new ReadOnlyObjectWrapper<String>("Wine");
+                    case "BEER":
+                        return new ReadOnlyObjectWrapper<String>("Beer");
+                    default:
+                        return new ReadOnlyObjectWrapper<String>("N/A");
+                }
+
             }
         });
         contentCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<SubmittedApplication, String>, ObservableValue<String>>() {
@@ -112,21 +127,7 @@ public class SearchController {
             return row;
         });
 
-        List<SubmittedApplication> resultsList = search.searchApprovedApplications();
-        possibleSuggestions.clear();
-        Collections.sort(resultsList, new Comparator<SubmittedApplication>() {
-            @Override
-            public int compare(SubmittedApplication lhs, SubmittedApplication rhs) {
-                return lhs.getApplication().getAlcohol().getBrandName().compareToIgnoreCase(rhs.getApplication().getAlcohol().getBrandName());
-            }
-        });
-
-        for(SubmittedApplication application: resultsList){
-            possibleSuggestions.add(application.getApplication().getAlcohol().getBrandName());
-            possibleSuggestions.add(application.getApplication().getAlcohol().getName());
-        }
-
-        autoCompletionBinding = TextFields.bindAutoCompletion(searchField, possibleSuggestions);
+        //autoCompletionBinding = TextFields.bindAutoCompletion(searchField, possibleSuggestions);
 
 
         /*searchField.setOnKeyPressed(new EventHandler<KeyEvent>() {
@@ -139,7 +140,7 @@ public class SearchController {
 
             }
         });*/
-
+        refreshSuggestions();
         searchField.setText(searchTerm);
         search(searchTerm);
     }
@@ -152,6 +153,9 @@ public class SearchController {
         //delay is required for .getText() to get the updated field
         Platform.runLater(() -> {
             search(searchField.getText());
+
+
+
         });
     }
     public void search(String searchTerm) {
@@ -160,18 +164,54 @@ public class SearchController {
 
         //Find & add matching applications
         List<SubmittedApplication> resultsList = search.searchByName(searchTerm.trim());
-        ;
+        filterList(resultsList);
         data.addAll(resultsList); //change to resultsList
         descriptionLabel.setText("Showing " + data.size() + " results for \"" + searchTerm + "\"");
         descriptionLabel.setVisible(true);
         saveBtn.setDisable(data.size() == 0);
         contextSaveBtn.setDisable(data.size() == 0);
     }
+    private void refreshSuggestions(){
+        List<SubmittedApplication> resultsList = search.searchApprovedApplications();
+        filterList(resultsList);
+        possibleSuggestions.clear();
+        /*
+        Collections.sort(resultsList, new Comparator<SubmittedApplication>() {
+            @Override
+            public int compare(SubmittedApplication lhs, SubmittedApplication rhs) {
+                return lhs.getApplication().getAlcohol().getBrandName().compareToIgnoreCase(rhs.getApplication().getAlcohol().getBrandName());
+            }
+        });
+        */
+        for(SubmittedApplication application: resultsList){
+            possibleSuggestions.add(application.getApplication().getAlcohol().getBrandName());
+            possibleSuggestions.add(application.getApplication().getAlcohol().getName());
+        }
 
+        if (autoCompletionBinding != null){
+            autoCompletionBinding.dispose();
+        }
+        autoCompletionBinding = TextFields.bindAutoCompletion(searchField, possibleSuggestions);
+    }
+    public void filter(ActionEvent e){
+        Platform.runLater(() -> {
+            refreshSuggestions();
+            search(e);
+
+
+
+        });
+
+    }
     public void goHome() {
         main.loadHomepage();
     }
 
+    private void filterList(List<SubmittedApplication> appList){
+        appList.removeIf(p -> (filterBeers.isSelected() && p.getApplication().getAlcohol().getAlcoholType() == AlcoholType.BEER));
+        appList.removeIf(p -> (filterWine.isSelected() && p.getApplication().getAlcohol().getAlcoholType() == AlcoholType.WINE));
+        appList.removeIf(p -> (filterSpirits.isSelected() && p.getApplication().getAlcohol().getAlcoholType() == AlcoholType.DISTILLEDSPIRITS));
+    }
     public void saveCSV(ActionEvent e) {
 
 
