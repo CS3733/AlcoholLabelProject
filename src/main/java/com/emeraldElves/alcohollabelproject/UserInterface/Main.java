6a2f937ccb5255735aaba56f4e493cea1c72d6a6
Kyle Corry <kylecorry31@gmail.com>
2017-04-13 09:47:36 -0400

    Fixed print scaling

--- src/main/java/com/emeraldElves/alcohollabelproject/UserInterface/Main.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/UserInterface/Main.java
@@ -4,10 +4,12 @@ import com.emeraldElves.alcohollabelproject.Data.AlcoholType;
 import com.emeraldElves.alcohollabelproject.Data.SubmittedApplication;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.print.PageLayout;
 import javafx.print.PrinterJob;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -37,8 +39,15 @@ public class Main extends Application {
         PrinterJob job = PrinterJob.createPrinterJob();
         if(job != null){
             job.showPrintDialog(stage);
+            PageLayout pageLayout = job.getPrinter().getDefaultPageLayout();
+            double scaleX = pageLayout.getPrintableWidth() / stage.getWidth();
+            double scaleY = pageLayout.getPrintableHeight() / stage.getHeight();
+            double minimumScale = Math.min(scaleX, scaleY);
+            Scale scale = new Scale(minimumScale, minimumScale);
+            stage.getScene().getRoot().getTransforms().add(scale);
             job.printPage(stage.getScene().getRoot());
             job.endJob();
+            stage.getScene().getRoot().getTransforms().add(new Scale(1/minimumScale, 1/minimumScale));
         }
     }
 
