7d664ca7eaabfe178e148dd42616454aa35235bf
danbmckay <dbmckay@wpi.edu>
2017-04-15 16:10:11 -0400

    Added superuserWorkflowControoler

---
+++ src/main/java/com/emeraldElves/alcohollabelproject/UserInterface/SuperUserWorkflowController.java
@@ -0,0 +1,16 @@
+package com.emeraldElves.alcohollabelproject.UserInterface;
+
+/**
+ * Created by Dan on 4/15/2017.
+ */
+public class SuperUserWorkflowController {
+
+    Main main;
+    public void init(Main main){
+        this.main = main;
+        //show all from current potential user database
+
+    }
+
+    public void goHome(){ main.loadHomepage();}
+}
