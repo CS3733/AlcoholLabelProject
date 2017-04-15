4a875dfd0142058c94003a90d7fd4e0ece06a272
danbmckay <dbmckay@wpi.edu>
2017-04-15 16:19:07 -0400

    fixed issue in storage

--- src/main/java/com/emeraldElves/alcohollabelproject/Data/Storage.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/Data/Storage.java
@@ -98,11 +98,11 @@ public class Storage {
             Log.console("Used existing AlcoholInfo table");
         }
         try{
-            database.createTable("NewApplicant"),
+            database.createTable("NewApplicant",
                     new Database.TableField("username", "VARCHAR (255) UNIQUE NOT NULL"),
                     new Database.TableField("password", "VARCHAR (255) NOT NULL"),
-                    new Database.TableField("type", "INTEGER NOT NULL"));//0 Man, 1 TTB
-            Log.console("Created new NewApplicant table");
+                    new Database.TableField("type", "INTEGER NOT NULL") //0 Man, 1 TTB
+            Log.console("Created new NewApplicant table"));
         }
         catch (SQLException e){
             Log.console("Used existing NewApplicant table");
