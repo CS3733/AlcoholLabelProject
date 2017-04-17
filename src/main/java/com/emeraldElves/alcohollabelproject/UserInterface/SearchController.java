da2747249b1954a233cd640e7bf03938a526f680
danbmckay <dbmckay@wpi.edu>
2017-04-17 14:43:17 -0400

    fixed?

--- src/main/java/com/emeraldElves/alcohollabelproject/UserInterface/SearchController.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/UserInterface/SearchController.java
@@ -127,7 +127,7 @@ public class SearchController {
 
 
             }
-        });
+        //});
         refreshSuggestions();
         searchField.setText(searchTerm);
         notifyObservers();
