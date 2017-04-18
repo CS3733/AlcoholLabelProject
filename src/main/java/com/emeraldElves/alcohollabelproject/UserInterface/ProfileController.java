5f71191073ab26cc2ff67287d87dc2e7b5e55497
jsbremner <jsbremner@wpi.edu>
2017-04-18 12:05:48 -0400

    added ProfileController

---
+++ src/main/java/com/emeraldElves/alcohollabelproject/UserInterface/ProfileController.java
@@ -0,0 +1,23 @@
+package com.emeraldElves.alcohollabelproject.UserInterface;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by Joe on 4/18/2017.
+ */
+public class ProfileController {
+    // FXML
+    @FXML
+    TextField representativeID;
+    @FXML
+    TextField permitNum;
+    @FXML
+    TextField address;
+    @FXML
+    TextField phoneNum;
+    @FXML
+    TextField emailAddress;
+
+    
+}
