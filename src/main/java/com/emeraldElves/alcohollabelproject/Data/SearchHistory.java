a64a80262b6f80076ce76832cc5f20fdfdaf5814
keionbis <keion96@gmail.com>
2017-04-18 17:10:40 -0400

    Done class just need to figure out how were implementing it

--- src/main/java/com/emeraldElves/alcohollabelproject/Data/SearchHistory.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/Data/SearchHistory.java
@@ -1,7 +1,18 @@
 package com.emeraldElves.alcohollabelproject.Data;
 
+import java.util.Stack;
+
 /**
  * Created by keionbis on 4/18/17.
  */
 public class SearchHistory {
+    Stack searchHistory = new Stack();
+
+    public void addMostRecentSearch(String searchText){
+        searchHistory.push(searchText);
+    }
+    
+    public String getMostRecentSearch(){
+        return (String) searchHistory.pop();
+    }
 }
