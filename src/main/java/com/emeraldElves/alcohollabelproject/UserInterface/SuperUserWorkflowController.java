f0e5c45ce9f661329540ffe18a221bd0161bef1e
keionbis <keion96@gmail.com>
2017-04-18 23:40:10 -0400

    Implemented Ui features

--- src/main/java/com/emeraldElves/alcohollabelproject/UserInterface/SuperUserWorkflowController.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/UserInterface/SuperUserWorkflowController.java
@@ -2,6 +2,8 @@ package com.emeraldElves.alcohollabelproject.UserInterface;
 
 import com.emeraldElves.alcohollabelproject.Data.PotentialUser;
 import com.emeraldElves.alcohollabelproject.Data.Storage;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 import java.util.List;
 
@@ -9,6 +11,8 @@ import java.util.List;
  * Created by Dan on 4/15/2017.
  */
 public class SuperUserWorkflowController {
+    @FXML
+    Label Name, Type, PhoneNumber, RepresentativeID, Email, Date, physicalAddress, permitNo;
     List<PotentialUser> users;
     PotentialUser UserforApproval;
     Main main;
@@ -17,6 +21,15 @@ public class SuperUserWorkflowController {
         this.main = main;
         this.UserforApproval = UserforApproval;
         //add to displayed table, copy from search functionality
+        Name.setText(UserforApproval.getName());
+        Type.setText(UserforApproval.getUserType().toString());
+        PhoneNumber.setText(UserforApproval.getPhoneNumber().getPhoneNumber());
+        RepresentativeID.setText(String.valueOf(UserforApproval.getRepresentativeID()));
+        Email.setText(UserforApproval.getEmail().getEmailAddress());
+        Date.setText(UserforApproval.getDate().toString());
+        physicalAddress.setText(UserforApproval.getAddress());
+        permitNo.setText(String.valueOf(UserforApproval.getPermitNum()));
+
     }
 
     public void Approve() {
@@ -28,8 +41,10 @@ public class SuperUserWorkflowController {
     public void Reject() {
         Storage.getInstance().deleteUser(UserforApproval);
         main.loadNewUserApplicationDisplayController();
+
     }
 
+  
 
     public void printPage(){
         main.printPage();
