536e4660793e1debdfde1e20576f74d58c59604c
[Elijah Eldredge] <[elijaheldredge@gmail.com]>
2017-04-22 16:39:35 -0400

    implements grids for images, creates controller for About Page

---
+++ src/main/java/com/emeraldElves/alcohollabelproject/UserInterface/AboutPageController.java
@@ -0,0 +1,24 @@
+package com.emeraldElves.alcohollabelproject.UserInterface;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+
+import javax.swing.text.Element;
+import javax.swing.text.html.ImageView;
+
+/**
+ * Created by elijaheldredge on 4/22/17.
+ */
+public class AboutPageController {
+    @FXML
+    ImageView imageID;
+
+    Image wilsonWong = new Image("file:images/wwong.jpg");
+    ImageView wilsonWongView = new ImageView((Element) wilsonWong);
+
+    public AboutPageController() {
+
+    }
+
+
+}
