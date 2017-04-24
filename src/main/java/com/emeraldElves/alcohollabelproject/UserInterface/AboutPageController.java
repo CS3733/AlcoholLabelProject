30d886ccc0acaa7fdb1285a9ef27cf80701974d5
keionbis <keion96@gmail.com>
2017-04-24 10:31:22 -0400

    Fixed bu implementing icontroller

--- src/main/java/com/emeraldElves/alcohollabelproject/UserInterface/AboutPageController.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/UserInterface/AboutPageController.java
@@ -1,23 +1,30 @@
 package com.emeraldElves.alcohollabelproject.UserInterface;
 
 import javafx.fxml.FXML;
-import javafx.scene.image.Image;
 
 import javax.swing.text.html.ImageView;
 
 /**
  * Created by elijaheldredge on 4/22/17.
  */
-public class AboutPageController {
+public class AboutPageController implements IController {
+    Main main;
     @FXML
     ImageView imageID;
 
-    Image wilsonWong = new Image("file:images/wwong.jpg");
+   // Image wilsonWong = new Image("file:images/wwong.jpg");
 //    ImageView wilsonWongView = new ImageView((Element) wilsonWong);
+   public AboutPageController() {
 
-    public AboutPageController() {
+   }
+   public void init(Bundle bundle){
+       this.init(bundle.getMain("main"));
+   }
 
+    public void init(Main main) {
+        this.main = main;
     }
+
 
 
 }
