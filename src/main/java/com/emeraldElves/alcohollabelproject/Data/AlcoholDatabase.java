46a45165f09df808380bf9b64fb396ae7a27f2b6
Kyle Corry <kylecorry31@gmail.com>
2017-04-30 19:13:25 -0400

    Also don't get an item from an empty list

--- src/main/java/com/emeraldElves/alcohollabelproject/Data/AlcoholDatabase.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/Data/AlcoholDatabase.java
@@ -9,10 +9,7 @@ import com.emeraldElves.alcohollabelproject.Log;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 /**
  * Created by elijaheldredge on 3/31/17.
@@ -186,7 +183,7 @@ public class AlcoholDatabase {
     public boolean submitApplication(SubmittedApplication application, String username) {
 
         if (AppState.getInstance().ttbAgents == null) {
-            AppState.getInstance().ttbAgents = new ApplicationAssigner(new LazyAssigner(), new ArrayList<>());
+            AppState.getInstance().ttbAgents = new ApplicationAssigner(new LazyAssigner(), Arrays.asList("PENDING"));
         }
         //making application type
         ApplicationType appType = application.getApplication().getApplicationType();
