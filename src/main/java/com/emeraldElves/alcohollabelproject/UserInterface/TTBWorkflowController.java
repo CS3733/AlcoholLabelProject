231a23760c1284bf940d2c7257be839b081bfec0
Kyle Corry <kylecorry31@gmail.com>
2017-04-30 19:35:36 -0400

    Allow selection by type

--- src/main/java/com/emeraldElves/alcohollabelproject/UserInterface/TTBWorkflowController.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/UserInterface/TTBWorkflowController.java
@@ -1,14 +1,12 @@
 package com.emeraldElves.alcohollabelproject.UserInterface;
 
 import com.emeraldElves.alcohollabelproject.Authenticator;
-import com.emeraldElves.alcohollabelproject.Data.DateHelper;
-import com.emeraldElves.alcohollabelproject.Data.Storage;
-import com.emeraldElves.alcohollabelproject.Data.SubmittedApplication;
-import com.emeraldElves.alcohollabelproject.Data.TTBAgentInterface;
+import com.emeraldElves.alcohollabelproject.Data.*;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
@@ -16,6 +14,7 @@ import org.apache.commons.lang3.StringEscapeUtils;
 
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Kylec on 4/18/2017.
@@ -37,6 +36,16 @@ public class TTBWorkflowController implements IController {
 
     private ObservableList<SubmittedApplication> data = FXCollections.observableArrayList();
     private TTBAgentInterface agentInterface;
+
+    @FXML
+    private CheckBox beer;
+
+    @FXML
+    private CheckBox wine;
+
+    @FXML
+    private CheckBox spirits;
+
 
     private Main main;
 
@@ -75,6 +84,14 @@ public class TTBWorkflowController implements IController {
 
     public void fetchApplications(){
         List<SubmittedApplication> unassigned = Storage.getInstance().getUnassignedApplications();
+
+        unassigned = unassigned.stream().filter((a) -> {
+            AlcoholType type = a.getApplication().getAlcohol().getAlcoholType();
+            return type.equals(AlcoholType.BEER) && beer.isSelected() ||
+                    type.equals(AlcoholType.WINE) && wine.isSelected() ||
+                    type.equals(AlcoholType.DISTILLEDSPIRITS) && spirits.isSelected();
+        }).collect(Collectors.toList());
+
         int numAssigned = agentInterface.getAssignedApplications().size();
         for (int i = numAssigned; i < 10 && unassigned.size() > 0; i++) {
             agentInterface.addApplication(unassigned.get(0));
