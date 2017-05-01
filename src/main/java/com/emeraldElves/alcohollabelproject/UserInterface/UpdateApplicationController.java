f8ea098784d4ac149b07de8227cc8c2f953ccd77
Jessie Ying <jessie.ying@gmail.com>
2017-04-30 21:29:14 -0400

    disabled some fields

--- src/main/java/com/emeraldElves/alcohollabelproject/UserInterface/UpdateApplicationController.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/UserInterface/UpdateApplicationController.java
@@ -67,21 +67,17 @@ public class UpdateApplicationController implements IController{
     private String username;
     private ApplicantInterface applicant;
     private EmailAddress emailAddress;
-    private String permitNum; //needs to be a string!!!!
-    private String address;
+    private String permitNum, address;
     private PhoneNumber phoneNum;
     private int representativeID; //needs to be a string!!!!
 
     //Alcohol info
     private ProductSource pSource;
     private AlcoholType alcType;
-    private String alcName;
-    private String brandName;
+    private String alcName, brandName;
     private double alcContent;
     private AlcoholInfo.Wine wineType = null;
-    private String formula;
-    private String serialNum; //needs to be a string!!!!
-    private String extraInfo;
+    private String formula, serialNum, extraInfo;
     private File file;
 
     public void init(Bundle bundle){
@@ -329,7 +325,6 @@ public class UpdateApplicationController implements IController{
             formFilled = true;
         }
 
-
         //check if fields are valid
         if (isDouble(alcoholContentField)) {
             if (pTypeSelect.getValue().equals("Wine")) {
@@ -375,6 +370,9 @@ public class UpdateApplicationController implements IController{
     }
 
     public void disableAllFields() {
+        certOfApproval.setDisable(true);
+        certOfExemption.setDisable(true);
+        distinctiveApproval.setDisable(true);
         pSourceSelect.setDisable(true);
         pTypeSelect.setDisable(true);
         alcoholName.setDisable(true);
